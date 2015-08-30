12cc32ad99e26cc751504f0444d774b4b1a3864e
keilin.olsen <keilin.olsen@heathwallace.com>
2015-08-30 16:09:19 +1000

    splash screen binniz

---
+++ Assets/AutoRotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoRotate : MonoBehaviour {
+	public float rotationSpeed = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (Vector3.up, rotationSpeed);
+	}
+}
