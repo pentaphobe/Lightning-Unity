5a217bd3cf1712cfddd60980a0883176d314a023
Patrick Marabeas <patrick@marabeas.io>
2015-08-30 15:23:01 +1000

    Added player scores and start counter

---
+++ Assets/PlayerScore.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerScore : MonoBehaviour {
+
+	public Text scoreText;
+	public int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		SetScore ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SetScore ();
+	}
+
+	void SetScore () {
+		scoreText.text = score.ToString ();
+	}
+}
---
+++ Assets/StartCount.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartCount : MonoBehaviour {
+
+	public int count = 3;
+	public Text countText;
+	public bool canShoot = false;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("UpdateCount", 1, 1F);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private void UpdateCount() {
+		if(count > -1)
+			count--;
+
+		UpdateCountText ();
+	}
+
+	private void UpdateCountText() {
+		if (count > 0)
+			countText.text = count.ToString ();
+		else if (count == 0)
+			countText.text = "GO";
+		else if (count == -1)
+			countText.text = "";
+	}
+
+}
