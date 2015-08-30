fbcc25764b8c527894a3ed852c91436bbe68c757
keilin.olsen <keilin.olsen@heathwallace.com>
2015-08-30 12:47:42 +1000

    Initial Commit

---
+++ Assets/MissileControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileControl : MonoBehaviour {
+	public string attackBind = "P1_Fire";
+	public string horizontalBind = "P1_Horizontal";
+	public string verticalBind = "P1_Vertical";
+	public float speed = 0.00001f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float rot = Input.GetAxis (horizontalBind);
+		transform.Rotate (Vector3.up, rot);
+
+		transform.Translate (transform.forward * speed * Time.fixedDeltaTime);
+	}
+}
